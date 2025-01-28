1c5f8f3d64c248d7079aceb2330610105a93e5b7
MarcoSac1 <marcosaccone8@gmail.com>
2025-01-28 12:38:02 +0100

    add Navbar component and integrate it into App; remove header content

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import './Navbar.css';
+
+function NavBar(){
+    return(
+        <ul>
+            <li>
+                Home
+            </li>
+            <li>
+                Chi siamo
+            </li>
+            <li>
+                Consulenza
+            </li>
+            <li>
+                Contatti
+            </li>
+        </ul>
+    )
+};
+
+export default NavBar;// esporto il componente 
