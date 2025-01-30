ded42358feef2626b71b66f5b8263a7135808830
MarcoSac1 <marcosaccone8@gmail.com>
2025-01-30 12:40:56 +0100

    refactor Main component: integrate Card rendering and update styles; remove Main.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,16 @@
 import './App.css';
 import Header from'./components/Header';
 import Main from './components/Main';
-import Card from './components/Card.jsx';
 import Footer from './components/Footer';
 
 
 function App() {
 
-  const people = [
-    { title: 'Marco', description: 'lorem', imgUrl: 'https://i.redd.it/ys546mxqmrfe1.jpeg' ,isVisited:false,isAdmin:true },
-    { title: 'Elisa', description: 'Description 2', imgUrl: 'https://preview.redd.it/a-few-samples-of-my-past-and-future-profile-pics-v0-j4wjtg49mfce1.jpg?width=474&format=pjpg&auto=webp&s=64c9da2232cefeda62048b2202a43fc6dbca4022',isVisited:true,isAdmin:false },
-    // Aggiungi altre persone qui
-  ];
-  
   return (
     <div className="App">
       <Header isLoggedIn={true}/>
       <Header isLoggedIn={false}/>
       <Main/>
-      {people.map((person, index) => (
-        
-        <Card
-        key={index}
-        title={person.title}
-        description={person.description}
-        imgUrl={person.imgUrl}
-        isVisited={person.isVisited}
-        isAdmin ={person.isAdmin}
-        />
-      ))}
       <Footer/>
     </div>
   );
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,32 @@
-import'./Main.css';
-
+import '../App.css';
+import Card from './Card.jsx';
 
 function Main(){
+    const people = [
+        { title: 'Marco', description: 'lorem', imgUrl: 'https://i.redd.it/ys546mxqmrfe1.jpeg' ,isVisited:false,isAdmin:true },
+        { title: 'Elisa', description: 'Description 2', imgUrl: 'https://preview.redd.it/a-few-samples-of-my-past-and-future-profile-pics-v0-j4wjtg49mfce1.jpg?width=474&format=pjpg&auto=webp&s=64c9da2232cefeda62048b2202a43fc6dbca4022',isVisited:true,isAdmin:false },
+        // Aggiungi altre persone qui
+    ];
     return(
         <>
         <div class='main-content'>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere quisquam commodi tenetur iste consectetur, corrupti deserunt temporibus voluptates aperiam placeat ex aut facilis magni fugiat quo quidem quibusdam quasi pariatur.</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere quisquam commodi tenetur iste consectetur, corrupti deserunt temporibus voluptates aperiam placeat ex aut facilis magni fugiat quo quidem quibusdam quasi pariatur.</p>
+
         </div>
+        <section class='card-section'>
+
+        {people.map((person, index) => (
+            <Card
+            key={index}
+            title={person.title}
+            description={person.description}
+            imgUrl={person.imgUrl}
+            isVisited={person.isVisited}
+            isAdmin ={person.isAdmin}
+            />
+        ))}
+        </section>
         </>
     )
 };
